05396a17cc2256c608018488fa3f28b69a120d4d
ZARROUK Anis <anis.zarrouk@witik.io>
2025-05-14 08:12:19 +0100

    add cors

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express= require('express');
 const itemRoutes = require('./routes/items');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors());
 app.use('/api/items', itemRoutes);
 app.use(errorHandler);
 app.listen(port, () => console.log(`server is running on port ${port}...`));
